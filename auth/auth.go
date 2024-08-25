b7e4c594552813fda9f649c3476042149f136f49
ChanchalS7 <chsverma7@gmail.com>
2024-08-25 22:22:16 +0530

    Fixed token generating issue

--- auth/auth.go
+++ auth/auth.go
@@ -13,34 +13,39 @@ type Claims struct {
 	Email string   `json:"email"`
 	jwt.StandardClaims
 }
-//GenerateJWT generates jwt token
 
+//GenerateJWT generates jwt token
 func GenerateJWT(email string)(string, error){
-	expirtationTime := time.Now().Add(24 * time.Hour)
+	expirationTime := time.Now().Add(24 * time.Hour)
 
 	claims:= &Claims {
 		Email:email,
 		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: expirtationTime.Unix(),
+			ExpiresAt: expirationTime.Unix(),
 		},
 
 	}
-	token:= jwt.NewWithClaims(jwt.SigningMethodES256,claims)
-	return token.SignedString(jwtKey)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		return token.SignedString(jwtKey)
 
 }
 
 //ValidateJWT validates the JWT tokens
 func ValidateJWT (tokenStr string) (*Claims, error) {
 claims := &Claims{}
-	token, err := jwt.ParseWithClaims(tokenStr, claims, func( token *jwt.Token) (interface{}, error) {
-		return jwtKey,nil 
-	})
-	if err != nil {
-		return nil, err 
+	
+token, err := jwt.ParseWithClaims(tokenStr, claims,func(token *jwt.Token)(interface{},error){
+	//Ensure the tokens signing method is what you expect
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, jwt.NewValidationError("Unexpected signing method",jwt.ValidationErrorSignatureInvalid)
 	}
-	if !token.Valid {
-			return nil, jwt.NewValidationError("Token is invalid", jwt.ValidationErrorSignatureInvalid)
-	}
-	return claims, nil 
+	return jwtKey, nil
+})
+if err != nil {
+	return nil, err 
+}
+if !token.Valid {
+	return nil, jwt.NewValidationError("Token is invalid", jwt.ValidationErrorSignatureInvalid)
+}
+return claims, nil 
 }
